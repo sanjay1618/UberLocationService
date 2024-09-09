e7ca0e84392ed7e51ac8a55da625a439e4bda2fe
Sanjay <sanjay.uppugandla@gmail.com>
2024-09-09 14:26:44 -0700

    created empty location repo with redis and mongodb dependencies

---
+++ src/main/java/com/example/uberlocationservice/UberLocationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.uberlocationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberLocationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UberLocationServiceApplication.class, args);
+    }
+
+}
